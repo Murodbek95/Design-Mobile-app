29d2c6a962f466b3d4e9b0d84e13fe17d8c9d524
codemachine <maxalphaengineer@gmail.com>
2018-07-12 09:00:22 +0500

    Offline navigation bar and splash screen

---
+++ app/src/main/java/com/example/dellgaming/myapplication/SplashScreen.java
@@ -0,0 +1,23 @@
+package com.example.dellgaming.myapplication;
+
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.TimeUnit;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //  setContentView(R.layout.activity_splash_screen);
+//        SystemClock.sleep(TimeUnit.SECONDS.toMillis(3));
+        Intent intent = new Intent(this, offline.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+}
