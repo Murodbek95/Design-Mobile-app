6e8e008810ae9e597881e7f0655d0f0dd9d1f28c
codemachine <maxalphaengineer@gmail.com>
2018-08-17 14:27:06 +0500

    Oflline home edited design

---
+++ app/src/main/java/com/example/dellgaming/myapplication/extra/CHatFragment.java
@@ -0,0 +1,30 @@
+package com.example.dellgaming.myapplication.extra;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dellgaming.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CHatFragment extends Fragment {
+
+
+    public CHatFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+}
