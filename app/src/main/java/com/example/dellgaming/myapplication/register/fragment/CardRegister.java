0cfa60f0b126dad7ffd4c033d5da68df6630ff0f
MM13ll <magnus13maxwell@gmail.com>
2018-09-10 16:19:47 +0500

    Userprofile Online activity calling. + Sending json post to obtain code via sms in background task.

--- app/src/main/java/com/example/dellgaming/myapplication/register/fragment/CardRegister.java
+++ app/src/main/java/com/example/dellgaming/myapplication/register/fragment/CardRegister.java
@@ -1,15 +1,30 @@
 package com.example.dellgaming.myapplication.register.fragment;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.example.dellgaming.myapplication.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -33,7 +48,23 @@ public class CardRegister extends Fragment {
 //                Intent intent = new Intent(offline.this,
 //                        CardRegisterAct.class);
 //                startActivity(intent);
+                JSONObject post_dict = new JSONObject();
+
+                try {
+                    post_dict.put("text", "Telegram-Bot\nKod aktivatsii: 123");
+                    post_dict.put("phone", "998909246463");
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (post_dict.length() > 0) {
+                    new ExecuteTask().execute(String.valueOf(post_dict));
+                }
+
+
                 System.out.println("checkline button");
+
+
                 BlankFragment fragment = new BlankFragment();
                 FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
                 transaction.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out);
@@ -55,5 +86,82 @@ public class CardRegister extends Fragment {
         // Inflate the layout for this fragment
 
     }
+    class ExecuteTask extends AsyncTask<String, Integer, String>{
+        private static final String TAG = "ERROR";
+
+        @Override
+        protected String doInBackground(String... params) {
+            String JsonResponse = null;
+            String JsonDATA = params[0];
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            try {
+                URL url = new URL("http://test.inlab.uz/test/testmobileappjson.php");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                // is output buffer writter
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+//set headers and method
+                Writer writer = new BufferedWriter(new OutputStreamWriter(urlConnection.getOutputStream(), "UTF-8"));
+                writer.write(JsonDATA);
+// json data
+                writer.close();
+                InputStream inputStream = urlConnection.getInputStream();
+//input stream
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null)
+                    buffer.append(inputLine + "\n");
+                if (buffer.length() == 0) {
+                    // Stream was empty. No point in parsing.
+                    return null;
+                }
+                JsonResponse = buffer.toString();
+//response data
+                Log.i(TAG,JsonResponse);
+                try {
+//send to post execute
+                    return JsonResponse;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String s) {
+        }
+
+    }
+
 
 }
+
